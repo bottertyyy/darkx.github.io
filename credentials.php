453b7705d1bcfdc9f17c4f6d81e585f910283f3e
bottertyyy <98246709+bottertyyy@users.noreply.github.com>
2023-02-07 04:57:55 +0100

    remove

--- credentials.php
+++
@@ -1,5 +0,0 @@
-<?php
-$name = "hwid"; // Application name
-$OwnerId = "ARXyIZvai7"; // Application ownerID
-$SellerKey = "36b14ca413fd76eabc271ab65799886d"; // application's seller key. Found in seller settings (You must have seller plan)
-?>
