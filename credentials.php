9e8475db9678d6613e4df4122d829e5ac5a2e52c
bottertyyy <98246709+bottertyyy@users.noreply.github.com>
2023-02-07 04:45:17 +0100

    Added new files

---
+++ credentials.php
@@ -0,0 +1,5 @@
+<?php
+$name = "hwid"; // Application name
+$OwnerId = "ARXyIZvai7"; // Application ownerID
+$SellerKey = "36b14ca413fd76eabc271ab65799886d"; // application's seller key. Found in seller settings (You must have seller plan)
+?>
